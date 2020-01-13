535b26f34c8a61eb60638593737a709145a72128
codedrinker <gwangchunlei@gmail.com>
2020-01-13 21:15:27 +0800

    complete Majiang p31

--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -25,6 +25,7 @@ public class QuestionService {
         PaginationDTO paginationDTO = new PaginationDTO();
         Integer totalCount = questionMapper.count();
         Integer totalPage = (totalCount + size - 1) / size;
+
         if (page < 1) {
             page = 1;
         }
@@ -77,5 +78,14 @@ public class QuestionService {
 
         paginationDTO.setQuestions(questionDTOList);
         return paginationDTO;
+    }
+
+    public QuestionDTO getById(Integer id) {
+        Question question = questionMapper.getById(id);
+        QuestionDTO questionDTO = new QuestionDTO();
+        BeanUtils.copyProperties(question, questionDTO);
+        User user = userMapper.findById(question.getCreator());
+        questionDTO.setUser(user);
+        return questionDTO;
     }
 }
